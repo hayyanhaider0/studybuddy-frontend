0e22e2f532f46e85a7f95a479ee7acbf0db652b3
hayyanhaider0 <hayyan2003@gmail.com>
2025-12-06 00:47:58 -0600

    Frontend: Commenting on recent components

--- screens/FlashcardScreen.tsx
+++ screens/FlashcardScreen.tsx
@@ -1,3 +1,9 @@
+/**
+ * FlashcardScreen Component
+ *
+ * This screen renders each flashcard from the selected deck.
+ */
+
 import { Pressable, Text, View } from "react-native"
 import { useLLMContext } from "../features/llm/contexts/LLMContext"
 import { useState } from "react"
@@ -13,25 +19,30 @@ import { MotiView } from "moti"
 type FlashcardScreenRouteProp = RouteProp<FlashcardStackParamList, "flashcard">
 
 export default function FlashcardScreen({ route }: { route: FlashcardScreenRouteProp }) {
+	// Get context values and extract the current flashcard deck.
 	const { id } = route.params
 	const { flashcardState } = useLLMContext()
-	const [questionIndex, setQuestionIndex] = useState<number>(0)
-
-	const { theme, GlobalStyles } = useThemeContext()
-
 	const flashcard = flashcardState.flashcards.find((f) => f.id === id)
-	const [flipped, setFlipped] = useState<boolean[]>(
+
+	const [questionIndex, setQuestionIndex] = useState<number>(0) // Tracks the question index.
+	const [flipped, setFlipped] = useState<boolean[]>( // Tracks which of the cards are flipped.
 		flashcard ? new Array(flashcard.items.length).fill(false) : []
 	)
 	if (!flashcard) return
 
-	const currentQuestion = flashcard.items[questionIndex]
+	// Theming.
+	const { theme, GlobalStyles } = useThemeContext()
 
+	const currentQuestion = flashcard.items[questionIndex] // Tracks the current question.
+
+	// Increments the question.
 	const incrementQuestion = () =>
 		setQuestionIndex((prev) => (prev + 1 < flashcard.items.length ? prev + 1 : prev))
 
+	// Decrements the question.
 	const decrementQuestion = () => setQuestionIndex((prev) => (prev > 0 ? prev - 1 : prev))
 
+	// Flips the flashcard.
 	const flip = () => {
 		setFlipped((prev) => prev.map((isFlipped, i) => (i === questionIndex ? !isFlipped : isFlipped)))
 	}
@@ -40,14 +51,10 @@ export default function FlashcardScreen({ route }: { route: FlashcardScreenRoute
 		<View style={[GlobalStyles.container, { padding: 16 }]}>
 			<CustomScrollView contentStyle={{ minHeight: "100%", gap: 32, alignItems: "center" }}>
 				<Pressable onPress={flip}>
+					{/* Flashcard Background */}
 					<MotiView
-						animate={{
-							rotateY: flipped[questionIndex] ? "180deg" : "0deg",
-						}}
-						transition={{
-							type: "timing",
-							duration: 600,
-						}}
+						animate={{ rotateY: flipped[questionIndex] ? "180deg" : "0deg" }}
+						transition={{ type: "timing", duration: 600 }}
 						style={{
 							backgroundColor: theme.colors.primary,
 							borderRadius: 32,
@@ -56,31 +63,35 @@ export default function FlashcardScreen({ route }: { route: FlashcardScreenRoute
 							gap: 16,
 						}}
 					>
+						{/* Card Inner Details */}
 						<MotiView
-							animate={{
-								rotateY: flipped[questionIndex] ? "180deg" : "0deg",
-							}}
-							transition={{
-								type: "timing",
-								duration: 600,
-							}}
+							animate={{ rotateY: flipped[questionIndex] ? "180deg" : "0deg" }}
+							transition={{ type: "timing", duration: 600 }}
 							style={{ gap: 8 }}
 						>
+							{/* Current Question */}
 							<Text style={GlobalStyles.subheading}>{currentQuestion.question}</Text>
+
+							{/* Flipped Details */}
 							<MotiView animate={{ opacity: flipped[questionIndex] ? 1 : 0 }} style={{ gap: 8 }}>
 								<HorizontalRule />
+								{/* Answer */}
 								<GradientText
 									text={currentQuestion.answer}
 									style={[GlobalStyles.subheading, { fontWeight: 100 }]}
 								/>
 								<HorizontalRule />
+								{/* Explanation */}
 								<Text style={GlobalStyles.paragraph}>{currentQuestion.explanation}</Text>
 							</MotiView>
+
+							{/* Hint telling the user to tap the card to flip. */}
 							<Text style={[GlobalStyles.subtext, { marginTop: "auto" }]}>Tap to flip.</Text>
 						</MotiView>
 					</MotiView>
 				</Pressable>
 
+				{/* Bottom Bar */}
 				<View
 					style={{
 						flexDirection: "row",
@@ -90,10 +101,15 @@ export default function FlashcardScreen({ route }: { route: FlashcardScreenRoute
 						marginTop: "auto",
 					}}
 				>
+					{/* Back Button */}
 					<CustomPressable type='secondary' title='Back' onPress={decrementQuestion} />
+
+					{/* Question Tracker: current / total */}
 					<Text style={GlobalStyles.paragraph}>
 						{questionIndex + 1} / {flashcard.items.length}
 					</Text>
+
+					{/* Next Button */}
 					<CustomPressable type='primary' title='Next' onPress={incrementQuestion} />
 				</View>
 			</CustomScrollView>
--- screens/QuizScreen.tsx
+++ screens/QuizScreen.tsx
@@ -1,3 +1,9 @@
+/**
+ * Quiz Screen
+ *
+ * This screen renders each question from the selected quiz.
+ */
+
 import { Pressable, Text, View } from "react-native"
 import { useLLMContext } from "../features/llm/contexts/LLMContext"
 import { useState } from "react"
@@ -15,38 +21,49 @@ import { MotiView } from "moti"
 type QuizScreenRouteProp = RouteProp<QuizStackParamList, "quiz">
 
 export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
+	// Get context values and extract the selected quiz.
 	const { id } = route.params
 	const { quizState } = useLLMContext()
 	const { openModal } = useModal()
-	const [questionIndex, setQuestionIndex] = useState<number>(0)
-
-	const { theme, GlobalStyles } = useThemeContext()
-
 	const quiz = quizState.quizzes.find((q) => q.id === id)
 	if (!quiz) return
 
-	const [selectedIndeces, setSelectedIndeces] = useState<number[]>(
+	// Theming.
+	const { theme, GlobalStyles } = useThemeContext()
+
+	const [questionIndex, setQuestionIndex] = useState<number>(0) // Tracks the selected question.
+	const [selectedIndeces, setSelectedIndeces] = useState<number[]>( // Tracks the user selected options.
 		new Array(quiz.items.length).fill(-1)
 	)
-	const [showExplanation, setShowExplanation] = useState<boolean[]>(
+	const [showExplanation, setShowExplanation] = useState<boolean[]>( // Tracks which answers have been revealed.
 		new Array(quiz.items.length).fill(false)
 	)
 
-	const currentQuestion = quiz.items[questionIndex]
+	const currentQuestion = quiz.items[questionIndex] // Tracks the current question.
 
+	// Increments the question.
 	const incrementQuestion = () =>
 		setQuestionIndex((prev) => (prev + 1 < quiz.items.length ? prev + 1 : prev))
 
+	// Decrements the question.
 	const decrementQuestion = () => setQuestionIndex((prev) => (prev > 0 ? prev - 1 : prev))
 
+	// Selects some option in the question.
 	const selectOption = (index: number) => {
 		setSelectedIndeces((prev) => prev.map((v, i) => (i === questionIndex ? index : v)))
 	}
 
+	/**
+	 * Opens a modal to submit the quiz.
+	 *
+	 * A confirmation modal is opened which tells the user if they missed answering
+	 * any questions.
+	 */
 	const submit = () => {
-		let missing = 0
-		selectedIndeces.forEach((i) => (i === -1 ? missing++ : 0))
+		let missing = 0 // Initialize missing answers count.
+		selectedIndeces.forEach((i) => (i === -1 ? missing++ : 0)) // Track how many answers are missing.
 
+		// Open the modal.
 		openModal({
 			type: "confirm",
 			title: "Submit Quiz",
@@ -55,17 +72,21 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 			}`,
 			buttonText: "Submit",
 			onSubmit: () => {
-				let correct = 0
+				let correct = 0 // Initialize correct answers count.
 				selectedIndeces.forEach((selected, i) => {
+					// Track how many answers are correct.
 					if (quiz.items[i].answer === quiz.items[i].options[selected]) correct++
 				})
+				// LOG THE SCORE TEMP
 				console.log("Score:", correct, "/", quiz.items.length)
+				// Reset
 				setSelectedIndeces(new Array(quiz.items.length).fill(-1))
 				setShowExplanation(new Array(quiz.items.length).fill(true))
 			},
 		})
 	}
 
+	// Toggle answer (Reveal/Hide answer).
 	const toggleAnswer = () => {
 		setShowExplanation((prev) => prev.map((val, i) => (i === questionIndex ? !val : val)))
 	}
@@ -73,10 +94,13 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 	return (
 		<View style={[GlobalStyles.container, { padding: 16 }]}>
 			<CustomScrollView contentStyle={{ minHeight: "100%", gap: 16 }}>
+				{/* Question */}
 				<Text style={[GlobalStyles.subheading, { textAlign: "left" }]}>
 					{questionIndex + 1}. {currentQuestion.question}
 				</Text>
 				<HorizontalRule />
+
+				{/* Options */}
 				<View style={{ gap: 8 }}>
 					{currentQuestion.options.map((opt, i) => {
 						return (
@@ -93,6 +117,7 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 									borderRadius: 999,
 								}}
 							>
+								{/* Show the selected option */}
 								<MaterialC
 									name={selectedIndeces[questionIndex] === i ? "radiobox-marked" : "radiobox-blank"}
 									size={12}
@@ -104,6 +129,7 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 					})}
 				</View>
 
+				{/* Bottom Bar */}
 				<View
 					style={{
 						flexDirection: "row",
@@ -111,10 +137,16 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 						alignItems: "center",
 					}}
 				>
+					{/* Back Button */}
 					<CustomPressable type='secondary' title='Back' onPress={decrementQuestion} />
+
+					{/* Question Tracker: current / total */}
 					<Text style={GlobalStyles.paragraph}>
 						{questionIndex + 1} / {quiz.items.length}
 					</Text>
+
+					{/* Next/Submit button */}
+					{/* Submit button shows on the last question */}
 					{questionIndex === quiz.items.length - 1 ? (
 						<CustomPressable type='primary' title='Submit' onPress={submit} />
 					) : (
@@ -122,6 +154,7 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 					)}
 				</View>
 
+				{/* Reveal/Hide Answer Button */}
 				<CustomPressable
 					type='secondary'
 					title={showExplanation[questionIndex] ? "Hide Answer" : "Reveal Answer"}
@@ -132,6 +165,8 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 					}}
 				/>
 
+				{/* Answer and Explanation */}
+				{/* Only appears when the user reveals an answer. */}
 				<MotiView
 					from={{ opacity: 0 }}
 					animate={{ opacity: showExplanation[questionIndex] ? 1 : 0 }}
@@ -139,7 +174,11 @@ export default function QuizScreen({ route }: { route: QuizScreenRouteProp }) {
 					style={{ gap: 16, marginBottom: 8 }}
 				>
 					<HorizontalRule />
+
+					{/* Answer */}
 					<GradientText text={currentQuestion.answer} style={GlobalStyles.subheading} />
+
+					{/* Explanation */}
 					<Text style={GlobalStyles.paragraph}>{currentQuestion.explanation}</Text>
 					<HorizontalRule />
 				</MotiView>
